76ba13c880154013dc9292a20ab64e14fe868b92
Owen Hallgren <owenhallgren@gmail.com>
2021-06-08 12:08:52 -0600

    Basic configuration

---
+++ src/components/App/App.js
@@ -0,0 +1,48 @@
+import { Component } from 'react'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  
+  render() {
+    return (
+      <div className="home-page">
+        <div>Which category do you want to look at today!</div>
+        <button>arts</button>
+        <button>automobiles</button>
+        <button>books</button>
+        <button>business</button>
+        <button>fashion</button>
+        <button>food</button>
+        <button>health</button>
+        <button>home</button>
+        <button>insider</button>
+        <button>magazine</button>
+        <button>movies</button>
+        <button>nyregion</button>
+        <button>obituaries</button>
+        <button>opinion</button>
+        <button>politics</button>
+        <button>realestate</button>
+        <button>science</button>
+        <button>sports</button>
+        <button>sundayreview</button>
+        <button>technology</button>
+        <button>theater</button>
+        <button>magazine</button>
+        <button>travel</button>
+        <button>upshot</button>
+        <button>us</button>
+        <button>world</button>
+      </div>
+      
+
+    )
+  }
+}
+export default App;
