81105d26fbee7bac2839ef05aad04e2d6816a5df
Owen Hallgren <owenhallgren@gmail.com>
2021-06-09 10:24:59 -0600

    Added api call to fetch articles

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,4 @@
-export const fetchArticles = (type) => {
-  return fetch(`https://api.nytimes.com/svc/topstories/v2/${type}.json?api-key=2rvh1cCWJXvhMLlWSgPBfnJe7Tb5JsLL`)
+export const fetchArticles = (genre) => {
+  return fetch(`https://api.nytimes.com/svc/topstories/v2/${genre}.json?api-key=2rvh1cCWJXvhMLlWSgPBfnJe7Tb5JsLL`)
   .then(res => res.json())
 }
