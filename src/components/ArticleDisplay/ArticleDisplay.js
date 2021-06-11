b320cf56bf2c89c294e4c5798e594e6c791b479a
Owen Hallgren <owenhallgren@gmail.com>
2021-06-10 22:23:34 -0600

    Changed way filter works

--- src/components/ArticleDisplay/ArticleDisplay.js
+++ src/components/ArticleDisplay/ArticleDisplay.js
@@ -1,28 +1,32 @@
 import './ArticleDisplay.css'
 import ArticleCard from '../ArticleCard/ArticleCard.js'
 import FilterForm from '../FilterForm/FilterForm.js'
+import { Link } from 'react-router-dom'
 
-const ArticleDisplay = ({articles}) => {
-  let articlesToDisplay = articles.map(article => {
+const ArticleDisplay = ({articles, filteredArticles, filterArticles}) => {
+  let articlesToMap = filteredArticles.length ? filteredArticles : articles
+  let articlesToDisplay = articlesToMap.map(article => {
       return (
           <ArticleCard article = {article} />
       ) 
     })
 
-    const filterArticles = (e, search) => {
-      e.preventDefault()
-      console.log('yesssir')
-      // let filteredArticles = articlesToDisplay.filter(article => article.title.includes(search) || article.abstract.includes(search))
-      // if(filteredArticles.length) {
-      //   articlesToDisplay = filteredArticles
-      // } 
-    }
+    // const filterArticles = (search) => {
+    //   console.log('yesssir')
+    //   // let filteredArticles = articlesToDisplay.filter(article => article.title.includes(search) || article.abstract.includes(search))
+    //   // if(filteredArticles.length) {
+    //   //   articlesToDisplay = filteredArticles
+    //   // } 
+    // }
     
    return(
-      <>
+      <div className="article-display-container">
+        <Link to={'/'} style={{textDecoration: 'none'}}>
+          <button className='home-button'>Home</button>
+        </Link>
         <FilterForm filterArticles={filterArticles} />
         <div className="article-container">{articlesToDisplay}</div>
-      </>
+      </div>
     )
 }
 
