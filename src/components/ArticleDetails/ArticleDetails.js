5839ced6813febb182210ac6f6df92581f1a160f
Owen Hallgren <owenhallgren@gmail.com>
2021-06-10 22:23:14 -0600

    Changed way props was passed

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -1,13 +1,18 @@
 import "./ArticleDetails.css"
+import { Link } from 'react-router-dom'
+
 
 const ArticleDetails = ({title, articles}) => {
   let articleToDisplay = articles.find(article => article.title === title)
   return (
-    <div>
+    <div className="details-container">
       <h1>{articleToDisplay.title}</h1>
       <h2>{articleToDisplay.abstract}</h2>
-      <a href={articleToDisplay.short_url}>Full Story</a>
+      <a href={articleToDisplay.short_url} className="full-story">Full Story</a>
       <img src={articleToDisplay.multimedia[0].url} className="photo" alt="related to article"/>
+      <Link to={'/articles'} style={{textDecoration: 'none'}}>
+        <button>Back</button>
+      </Link>
     </div>
   )
 }
